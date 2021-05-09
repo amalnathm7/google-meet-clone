0735af0f452b3c761619f8bc4d9c81f676a89816
amalnathm7 <amalnathm7@gmail.com>
2021-05-09 18:51:24 +0530

    commit 1

---
+++ android/app/src/main/kotlin/com/amalmeenu/gmeet/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.amalmeenu.gmeet
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meet',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
