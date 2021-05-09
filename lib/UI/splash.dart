0caa0ac2db77c3f77e1d1bfc59367ba6e9d6f565
amalnathm7 <amalnathm7@gmail.com>
2021-05-09 20:11:01 +0530

    splash screen

--- lib/UI/splash.dart
+++ lib/UI/splash.dart
@@ -11,7 +11,23 @@ class SplashState extends State<Splash>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      body: Center(
+        child: Image.asset("assets/logo.png",
+          height: 140,
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(bottom: 50),
+        child: Text(
+          "Google",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 35,
+            fontFamily: 'Product Sans',
+            color: Colors.grey[400],
+          )
+        ),
+      ),
     );
   }
 }
