84c28e828c91a0dfaba3afdeee4430e82b71f737
amalnathm7 <amalnathm7@gmail.com>
2021-05-16 16:54:29 +0530

    Splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:gmeet/Services/googleauth.dart';
 import 'package:gmeet/UI/home.dart';
-import 'package:gmeet/UI/splash.dart';
 import 'package:gmeet/UI/welcome.dart';
 
 void main() async {
@@ -15,20 +13,11 @@ void main() async {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StreamBuilder(
-        stream: FirebaseFirestore.instance.collection("users").snapshots(),
-        builder: (context, snapshot) {
-          return MaterialApp(
-              debugShowCheckedModeBanner: false,
-              title: 'Gmeet',
-              theme: ThemeData(
-                  accentColor: Colors.green[700],
-                  dividerColor: Colors.transparent),
-              home: snapshot.hasData
-                  ? GoogleAuth().userIn == null
-                      ? Welcome()
-                      : Home()
-                  : Splash());
-        });
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Gmeet',
+        theme: ThemeData(
+            accentColor: Colors.green[700], dividerColor: Colors.transparent),
+        home: GoogleAuth().userIn == null ? Welcome() : Home());
   }
 }
